e4062c6a0dd76fab9cac7280a0289ec5a7be0038
riskifajar <rhisfajar@gmail.com>
2018-04-18 00:44:00 +0700

    srcrolling

--- Resume Web/js/scrolling.js
+++ Resume Web/js/scrolling.js
@@ -1,78 +1,32 @@
-$(function () {
-    var $window = $(window),
-        $document = $(document),
-        transitionSupported = typeof document.body.style.transitionProperty === "string", // detect CSS transition support
-        scrollTime = 1; // scroll time in seconds
-
-    $(document).on("click", "a[href*=#]:not([href=#])", function (e) {
-        var target,
-            avail,
-            scroll,
-            deltaScroll;
-
-        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-            target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[id=" + this.hash.slice(1) + "]");
-
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
             if (target.length) {
-                avail = $document.height() - $window.height();
-
-                if (avail > 0) {
-                    scroll = target
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target
                         .offset()
-                        .top;
-
-                    if (scroll > avail) {
-                        scroll = avail;
-                    }
-                } else {
-                    scroll = 0;
-                }
-
-                deltaScroll = $window.scrollTop() - scroll;
-
-                // if we don't have to scroll because we're already at the right scrolling
-                // level,
-                if (!deltaScroll) {
-                    return; // do nothing
-                }
-
-                e.preventDefault();
-
-                if (transitionSupported) {
-                    $("html")
-                        .css({
-                        "margin-top": deltaScroll + "px",
-                        "transition": scrollTime + "s ease-in-out"
-                    })
-                        .data("transitioning", scroll);
-                } else {
-                    $("html, body").stop(true, true) // stop potential other jQuery animation (assuming we're the only one doing it)
-                        .animate({
-                        scrollTop: scroll + "px"
-                    }, scrollTime * 1000);
-
-                    return;
-                }
+                        .top
+                }, 1000, function () {
+                    // Callback after animation Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
             }
         }
     });
-
-    if (transitionSupported) {
-        $("html")
-            .on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function (e) {
-                var $this = $(this),
-                    scroll = $this.data("transitioning");
-
-                if (e.target === e.currentTarget && scroll) {
-                    $this
-                        .removeAttr("style")
-                        .removeData("transitioning");
-
-                    $("html, body").scrollTop(scroll);
-                }
-            });
-    }
-});
